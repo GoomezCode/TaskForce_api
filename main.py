053431e30a9c58e023f5771d19d2d582da6234a5
GoomezCode <danielgdpaula@gmail.com>
2026-07-06 22:27:01 -0300

    alteração para rodar em hospedagem

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 import uvicorn
 from api import apiTask
@@ -11,4 +12,5 @@ def raiz():
     return {"msg": "Api inicializada com sucesso...."}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info", reload=True)
